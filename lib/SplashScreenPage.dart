31c1bde99a936c9cc25138d3bd6417bb67f01c60
langgilang <gilangsafira1416@gmail.com>
2021-06-13 09:50:56 +0700

    Membuat SpashScreen

---
+++ lib/SplashScreenPage.dart
@@ -0,0 +1,42 @@
+import 'dart:async';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'Mulai.dart';
+
+class SplashScreenPage extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreenPage> {
+  @override
+  void initState() {
+    super.initState();
+    startSplashScreen();
+  }
+
+  startSplashScreen() async {
+    var duration = const Duration(seconds: 5);
+    return Timer(duration, () {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (_) {
+          return Mulai();
+        }),
+      );
+    });
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Image.asset(
+          "images/launch_image.png",
+          width: 500.0,
+          height: 400.0,
+        ),
+      ),
+    );
+  }
+}
